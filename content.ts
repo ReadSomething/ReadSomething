48b6250827259255ed22c34524f14da9955cb0da
Zhongyi <hi@daizhongyi.com>
2023-02-02 16:40:31 +0800

    Create project.

---
+++ content.ts
@@ -0,0 +1,16 @@
+import {Readability} from "@mozilla/readability";
+
+function ReadMode() {
+    const documentClone = document.cloneNode(true);
+    const article = new Readability(<Document>documentClone).parse();
+    console.log(article.title)
+    console.log(article.byline)
+    console.log(article.excerpt)
+    console.log(article.dir)
+    console.log(article.content)
+    console.log(article.textContent)
+
+
+}
+
+export default ReadMode
