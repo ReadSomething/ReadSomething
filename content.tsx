6b1f7cfbed445c91aef55ecdbd581ee97fe76142
Wacque <641666985@qq.com>
2023-02-03 17:59:33 +0800

    update reading time and author link

--- content.tsx
+++ content.tsx
@@ -2,101 +2,44 @@ import {Readability} from "@mozilla/readability";
 import $ from "jquery";
 import {useEffect, useState} from "react";
 import styleText from "data-text:./content.scss"
-import type { PlasmoGetStyle } from "plasmo"
+import type {PlasmoGetStyle} from "plasmo"
+import readingTime from 'reading-time/lib/reading-time'
 
+// plasmo 会调用这个方法
 export const getStyle: PlasmoGetStyle = () => {
     const style = document.createElement("style")
     style.textContent = styleText
     return style
 }
-// console.log(style)
-// export const getStyle: PlasmoGetStyle = () => {
-//     const style = document.createElement("style")
-//     style.textContent = styleText
-//     return style
-// }
 
+const getMetaContentByProperty = function (metaProperty: string) {
+    const metas = document.getElementsByTagName('meta');
 
-// function ReadMode() {
-//     launch()
-// }
-//
-// var latest_url;
-//
-// export function launch() {
-//     if (document.getElementById("cr-iframe") == null) {
-//         // Create iframe and append to body
-//         var iframe: HTMLElement = createIframe();
-//         document.body.appendChild(iframe);
-//
-//         latest_url = window.location.href;
-//         init();
-//     } else {
-//         iframe = document.getElementById("cr-iframe");
-//         if ($(iframe).is(':visible')) {
-//             $(iframe).fadeOut();
-//         } else {
-//             // Only parse the article if the url was changed
-//             if (latest_url == window.location) {
-//                 $(iframe).fadeIn();
-//             } else {
-//                 latest_url = window.location.href;
-//                 init();
-//             }
-//         }
-//     }
-// }
-//
-// function init() {
-//     // Initialize iframe & doc
-//     var iframe = document.getElementById('cr-iframe');
-//
-//     // Get parsed article
-//     const documentClone = document.cloneNode(true);
-//     const article = new Readability(documentClone as Document).parse();
-//
-//     var title = article.title;
-//     var content = article.content;
-//
-//     var article_url = window.location.href;
-//     if (article.byline == null) {
-//         var author = "Unknown author";
-//     } else {
-//         var author = article.byline;
-//     }
-//     console.log(content)
-//
-//     iframe.innerHTML = content
-//
-// }
-//
-// // Create iframe
-// function createIframe() {
-//     var iframe = document.createElement('div');
-//     iframe.id = "cr-iframe";
-//     iframe.style.height = "100%";
-//     iframe.style.width = "100%";
-//     iframe.style.position = "fixed";
-//     iframe.style.top = "0px";
-//     iframe.style.right = "0px";
-//     iframe.style.zIndex = "9000000000000000000";
-//     iframe.style.backgroundColor = "#fff";
-//
-//     return iframe;
-// }
-//
-// chrome.runtime.onMessage.addListener(
-//     function (request) {
-//         console.log('received')
-//         // chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
-//             launch()
-//         // })
-//     }
-// )
-//
-//
-// // launch()
-// // export default ReadMode
+    for (let i = 0; i < metas.length; i++) {
+        if (metas[i].getAttribute('property') === metaProperty) {
+            return metas[i].getAttribute('content');
+        }
+    }
+
+    return '';
+}
+
+const isValidUrl = urlString => {
+    try {
+        return Boolean(new URL(urlString));
+    } catch (e) {
+        return false;
+    }
+}
+
+function Author  ({link, author}: {link: string, author: string}) {
+    let authorNode = <span>{author}</span>
+
+    if (isValidUrl(link)) authorNode =
+        <a href={link} style={{color: 'inherit', textDecoration: 'none'}} target={'_blank'}>{author}</a>
+
+    return <div className="credits reader-credits">{authorNode}</div>
+}
 
 const Reader = () => {
     const [showReader, setShowReader] = useState(false);
@@ -123,41 +66,52 @@ const Reader = () => {
         }
     }, []);
 
-    if(!showReader) return null
+    if (!showReader) return null
 
     const documentClone = document.cloneNode(true);
     const article = new Readability(documentClone as Document).parse();
     const article_url = window.location.href;
     const author = article.byline ?? "Unknown author"
-    const estimatedReadTime = Math.max(Math.round(article.length / 1000), 1)
-    const estimatedReadTimeStr = `${estimatedReadTime} ${estimatedReadTime > 1 ? 'minutes' :  'minute'} read`
     const domain = window.location.hostname
+    const timeToReadStr = readingTime(article.textContent).text
+    const authorLink = getMetaContentByProperty('article:author')
+
+    console.log(article)
 
     // @ts-ignore
-    return  <div style={{"--font-size": "20px", "--content-width": "30em"}}  className={'ReadSomething dark sans-serif loaded'}>
-      <div style={{position: 'fixed', height: '100%', width: "100%", overflow: 'scroll', left: 0, top: 0, backgroundColor: "var(--main-background)"}}>
-          <div className={'container'} >
-              <div className="header reader-header reader-show-element">
-                  <a className="domain reader-domain"
-                     href={article_url}>{domain}</a>
-                  <div className="domain-border"></div>
-                  <h1 className="reader-title">{article.title}</h1>
-                  <div className="credits reader-credits">{author}</div>
-                  <div className="meta-data">
-                      <div className="reader-estimated-time" data-l10n-id="about-reader-estimated-read-time"
-                           data-l10n-args="{&quot;range&quot;:&quot;3–4&quot;,&quot;rangePlural&quot;:&quot;other&quot;}"
-                           dir="ltr">{estimatedReadTimeStr}
-                      </div>
-                  </div>
-              </div>
-              <hr/>
-              <div className={'content'}>
-                  <div className={`mozReaderContent readerShowElement`}>
-                      <div className='page' dangerouslySetInnerHTML={{__html: article.content}}/>
-                  </div>
-              </div>
-          </div>
-      </div>
+    return <div style={{"--font-size": "20px", "--content-width": "30em"}}
+                className={'ReadSomething dark sans-serif loaded'}>
+        <div style={{
+            position: 'fixed',
+            height: '100%',
+            width: "100%",
+            overflow: 'scroll',
+            left: 0,
+            top: 0,
+            backgroundColor: "var(--main-background)"
+        }}>
+            <div className={'container'}>
+                <div className="header reader-header reader-show-element">
+                    <a className="domain reader-domain"
+                       href={article_url}>{domain}</a>
+                    <div className="domain-border"></div>
+                    <h1 className="reader-title">{article.title}</h1>
+                    <Author link={authorLink} author={author}></Author>
+                    <div className="meta-data">
+                        <div className="reader-estimated-time" data-l10n-id="about-reader-estimated-read-time"
+                             data-l10n-args="{&quot;range&quot;:&quot;3–4&quot;,&quot;rangePlural&quot;:&quot;other&quot;}"
+                             dir="ltr">{timeToReadStr}
+                        </div>
+                    </div>
+                </div>
+                <hr/>
+                <div className={'content'}>
+                    <div className={`mozReaderContent readerShowElement`}>
+                        <div className='page' dangerouslySetInnerHTML={{__html: article.content}}/>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 }
 
