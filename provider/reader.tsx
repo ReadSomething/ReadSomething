0a2754491a443b31b2f7e4c02984b263e960d747
Zhongyi <hi@daizhongyi.com>
2023-02-16 18:59:57 +0800

    Add download to mardown feature.

--- provider/reader.tsx
+++ provider/reader.tsx
@@ -1,19 +1,43 @@
-import {createContext, ReactNode, useState} from "react";
+import { ReactNode, createContext, useState } from "react"
+
+export class Article {
+    title: string
+
+    constructor(title: string) {
+        this.title = title
+    }
+}
 
 interface TypeReaderContext {
     settingStatus: boolean
     setSettingStatus: (value: boolean) => void
+
+    article: Article
+
+    setArticle: (article: Article) => void
 }
 
 export const ReaderContext = createContext({} as TypeReaderContext)
 
-export function ReaderProvider({children}: { children: ReactNode }) {
-    const [settingStatus, setSettingStatus] = useState(false);
+export function ReaderProvider({
+    children,
+    article
+}: {
+    children: ReactNode
+    article: Article
+}) {
+    const [settingStatus, setSettingStatus] = useState(false)
+    const [_article, setArticle] = useState(article)
 
-    return <ReaderContext.Provider value={{
-        settingStatus,
-        setSettingStatus,
-    }}>
-        {children}
-    </ReaderContext.Provider>
+    return (
+        <ReaderContext.Provider
+            value={{
+                settingStatus,
+                setSettingStatus,
+                article: _article,
+                setArticle
+            }}>
+            {children}
+        </ReaderContext.Provider>
+    )
 }
