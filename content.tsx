f6a282787d2f03df1ee856aecfcad2402daea297
Zhongyi <hi@daizhongyi.com>
2023-02-17 19:21:40 +0800

    Add translate feature.

--- content.tsx
+++ content.tsx
@@ -153,7 +153,7 @@ function Main() {
                    <div
                         className={'ReadSomething'}>
                        <ThemeWrap>
-                           <div className={'fixed h-full  w-full overflow-scroll left-0 top-0'} style={{
+                           <div id={'readsomething-scroll'} className={'fixed h-full  w-full overflow-scroll left-0 top-0'} style={{
                                backgroundColor: "var(--main-background)"
                            }}>
                                 <ContainerWrap>
---
+++ utils/state.ts
@@ -0,0 +1,10 @@
+import type { Dispatch, SetStateAction } from "react";
+
+export const getLatestState = function<T>(dispatch: Dispatch<SetStateAction<T>>):Promise<T> {
+    return new Promise<T>( (resolve) => {
+        dispatch(prevState => {
+            resolve(prevState)
+            return prevState
+        })
+    })
+}
