74eccd3a832c591cb9a84935915252968f823eaf
Zhongyi <hi@daizhongyi.com>
2023-02-22 19:45:43 +0800

    Refact listener scroll event logic, use IntersectionObserver.

--- components/translate.tsx
+++ components/translate.tsx
@@ -1,9 +1,9 @@
-import React, { useContext, useEffect, useMemo, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import IconTranslate from "react:~/assets/translate.svg";
 import { translateAnchor, TRANSLATED_RESULT } from "~components/tranlator";
 import { getLatestState } from "~utils/state";
 import { ReaderContext } from "~provider/reader";
-import Tooltip from "./tooltip"
+import Tooltip from "./tooltip";
 import { SettingContext } from "~provider/setting";
 
 function Translate () {
@@ -11,110 +11,70 @@ function Translate () {
     const { translateOn, setTranslateOn } = useContext(ReaderContext);
     const { settingObject } = useContext(SettingContext);
 
-    const scrollListener = debounce(async () => {
-        await translateCurrentPage();
-    }, 200);
-
-    const listener = useMemo(() => scrollListener, []);
+    // fixme: singleton
+    const observer = new IntersectionObserver(
+        function (entries) {
+            for (const entry of entries) {
+                if (entry.isIntersecting) {
+                    void translateAnchor(entry.target, settingObject.translateService);
+                } else {
+                    console.log(entry.isIntersecting);
+                }
+            }
+        }
+    );
 
     const hideTranslateResult = function () {
         const translateResult = document.querySelectorAll("plasmo-csui")[0]
             .shadowRoot
-            .querySelectorAll<HTMLElement>(`[${TRANSLATED_RESULT}]`)
+            .querySelectorAll<HTMLElement>(`[${TRANSLATED_RESULT}]`);
 
         for (let i = 0; i < translateResult.length; i++) {
-            translateResult[i].style.display = 'none'
+            translateResult[i].style.display = "none";
         }
-    }
+    };
 
     const showTranslateResult = function () {
         const translateResult = document.querySelectorAll("plasmo-csui")[0]
             .shadowRoot
-            .querySelectorAll<HTMLElement>(`[${TRANSLATED_RESULT}]`)
+            .querySelectorAll<HTMLElement>(`[${TRANSLATED_RESULT}]`);
 
         for (let i = 0; i < translateResult.length; i++) {
-            translateResult[i].style.display = 'block'
+            translateResult[i].style.display = "block";
         }
-    }
+    };
 
     const handleTranslateButtonClick = async function () {
         setTranslateOn(!translateOn);
 
-        if (await getLatestState(setTranslateOn)) {
-            showTranslateResult()
+        const paragraphs = document.querySelectorAll("plasmo-csui")[0]
+            .shadowRoot
+            .querySelector("#readability-page-1")
+            .querySelectorAll("p:not(ul p, ol p), ul:not(ul ul, ol ul), ol:not(ol ol, ul ol)");
 
-            const paragraphs = document.querySelectorAll("plasmo-csui")[0]
-                .shadowRoot
-                .querySelector("#readability-page-1")
-                .querySelectorAll("p:not(ul p, ol p), ul:not(ul ul, ol ul), ol:not(ol ol, ul ol)");
+        if (await getLatestState(setTranslateOn)) {
+            showTranslateResult();
 
             setParagraphs(Array.from(paragraphs));
-
-            // first time
-            await translateCurrentPage();
-
-            // listen scroll event
-            const scroll = getScroll();
-            scroll.addEventListener("scroll", listener);
+            Array.from(paragraphs).map(e => observer.observe(e));
         } else {
-            hideTranslateResult()
-            const scroll = getScroll();
-            scroll.removeEventListener("scroll", listener);
-            console.log("Translate off. Remove scroll event listener.");
+            hideTranslateResult();
+            Array.from(paragraphs).map(e => observer.unobserve(e));
         }
     };
 
     useEffect(function () {
-        const scroll = getScroll();
-
         return function () {
-            scroll.removeEventListener("scroll", listener);
+            observer.disconnect();
+
         };
     });
-
-    function getScroll () {
-        return document.querySelectorAll("plasmo-csui")[0]
-            .shadowRoot
-            .querySelector("#readsomething-scroll");
-    }
-
-    const translateCurrentPage = async function () {
-        const _paragraphs = await getLatestState(setParagraphs);
-
-        for (let i = 0; i < _paragraphs.length; i++) {
-            const item = _paragraphs[i];
-            if (isInViewport(item)) void translateAnchor(item, settingObject.translateService);
-        }
-    };
-
-    function debounce (func, wait) {
-        let timeout;
-
-        return () => {
-            if (timeout) {
-                clearTimeout(timeout);
-            }
-
-            timeout = setTimeout(func, wait);
-        };
-    }
-
-    const isInViewport = function (elem) {
-        const bounding = elem.getBoundingClientRect();
-
-        return (
-            bounding.top >= 0 &&
-            bounding.left >= 0 &&
-            bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
-    };
 
     return (
         <div onClick={handleTranslateButtonClick}
             className={"setting fixed select-none right-[130px] top-[30px] select-none"}
         >
-            <Tooltip message={'Translate'}>
+            <Tooltip message={"Translate"}>
                 <button className={"outline-none"}>
                     <IconTranslate />
                 </button>
